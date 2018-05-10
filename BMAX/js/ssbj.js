e39859af503cda42b177fe9dab70cd7b4a9a8d45
1403025820@qq.com <1403025820@qq.com>
2018-05-10 12:00:07 +0800

    优化了左边栏过长，中间部分也会变长的bug

--- BMAX/js/ssbj.js
+++ BMAX/js/ssbj.js
@@ -4,3 +4,24 @@ function big(x){
 function normal(x){
     x.style.fontSize="14px";
 }
+function changeFrameHeight(){
+    var ifm= document.getElementById("center");
+    ifm.height=document.documentElement.innerHeight-54;
+}
+
+function ready(){
+    $("a").click(function () {
+        var ifm= document.getElementById("center");
+        ifm.height=document.documentElement.innerHeight-54;
+        var event= document.getElementById("newEvent");
+        event.height=ifm.height;
+    });
+}
+window.onresize=function(){
+    changeFrameHeight();
+    ready();
+}
+$(function(){
+    changeFrameHeight();
+    ready();
+});
