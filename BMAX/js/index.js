5cb0103fcb259e3b0885269bb547a1a2f75b2719
1403025820@qq.com <1403025820@qq.com>
2018-05-11 10:40:05 +0800

    隐藏了滚动轮，但是火狐不支持，有待优化

--- BMAX/js/index.js
+++ BMAX/js/index.js
@@ -69,5 +69,26 @@ function big(x){
 function normal(x){
     x.style.fontSize="14px";
 }
+$(document).ready(
+    function() {
+        $("#scro").niceScroll({
+            cursorcolor: "black", // 改变滚动条颜色，使用16进制颜色值
+            cursoropacitymin: 0, // 当滚动条是隐藏状态时改变透明度, 值范围 1 到 0
+            cursoropacitymax: 0, // 当滚动条是显示状态时改变透明度, 值范围 1 到 0
+            cursorwidth: "0px", // 滚动条的宽度，单位：便素
+            cursorborderradius: "5px", // 滚动条圆角（像素）
+            scrollspeed: 60, // 滚动速度
+            mousescrollstep: 40, // 鼠标滚轮的滚动速度 (像素)
+            touchbehavior: false, // 激活拖拽滚动
+        });
+    }
+);
 
+function changeFrameHeight(){
+    var ifm= document.getElementById("center");
+    ifm.height=document.documentElement.clientHeight-50;
+    ifm.width=document.documentElement.clientWidth-30;
+}
+window.onresize=function(){ changeFrameHeight();}
+$(function(){changeFrameHeight();});
 
