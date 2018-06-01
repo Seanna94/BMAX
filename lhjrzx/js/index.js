f7a4e7fd58c59b712d695943e035a617801a5c64
1403025820@qq.com <1403025820@qq.com>
2018-06-01 14:24:57 +0800

    龙湖金融中心demo

--- lhjrzx/js/index.js
+++ lhjrzx/js/index.js
@@ -1,68 +1,3 @@
-// var animateGlobal = "bounceIn";
-// var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-// //初始化动画
-// var initAnimate = function () {
-//         //按钮动画
-//         $("body").on("mouseover", ".btn", function () {
-//             var obj = $(this);
-//             if (obj.hasClass("animated") === false) {
-//                 obj.addClass("animated");
-//             }
-//             obj.addClass(animateGlobal).one(animationEnd, function () {
-//                 obj.removeClass(animateGlobal);
-//             });
-//         });
-//         //table_option动画
-//         $("body").on("mouseover", ".table_option", function () {
-//             var obj = $(this);
-//             if (obj.hasClass("animated") === false) {
-//                 obj.addClass("animated");
-//             }
-//             obj.addClass(animateGlobal).one(animationEnd, function () {
-//                 obj.removeClass(animateGlobal);
-//             });
-//         });
-//         //_frame_tab_tag动画
-//         $("body").on("mouseover", "._frame_tab_tag", function () {
-//             var obj = $(this);
-//             if (obj.hasClass("animated") === false) {
-//                 obj.addClass("animated");
-//             }
-//             obj.addClass(animateGlobal).one(animationEnd, function () {
-//                 obj.removeClass(animateGlobal);
-//             });
-//         });
-//         //dropdown-toggle
-//         $("body").on("mouseover", ".dropdown-toggle", function () {
-//             var obj = $(this);
-//             if (obj.hasClass("animated") === false) {
-//                 obj.addClass("animated");
-//             }
-//             obj.addClass(animateGlobal).one(animationEnd, function () {
-//                 obj.removeClass(animateGlobal);
-//             });
-//         });
-//         //_frame_left_menu
-//         $("body").on("mouseover", "#_frame_left_menu .treeview>a", function () {
-//             var obj = $(this);
-//             if (obj.hasClass("animated") === false) {
-//                 obj.addClass("animated");
-//             }
-//             obj.addClass(animateGlobal).one(animationEnd, function () {
-//                 obj.removeClass(animateGlobal);
-//             });
-//         });
-//         //_layout_header
-//         $("body").on("mouseover", "#_layout_header #topmenu_system_monitoring,#_layout_header .sidebar-toggle,#_layout_header a[data-toggle='control-sidebar']", function () {
-//             var obj = $(this);
-//             if (obj.hasClass("animated") === false) {
-//                 obj.addClass("animated");
-//             }
-//             obj.addClass(animateGlobal).one(animationEnd, function () {
-//                 obj.removeClass(animateGlobal);
-//             });
-//         });
-//     }
 function big(x){
     x.style.fontSize="16px";
 }
@@ -85,16 +20,21 @@ $(document).ready(
     }
 );
 
-function changeFrameHeight(){
+function changeFrameWidth(){
     var ifm= document.getElementById("center");
-    var sec=document.getElementById("scro");
-    var head=document.getElementById("head");
-    ht=document.documentElement.clientHeight-55;
-    wh=document.documentElement.clientWidth-55;
-    ifm.height=ht;
-    ifm.width=wh;
-    sec.height=ht;
+    ifm.width=$(document).width()-230;
 }
-window.onresize=function(){ changeFrameHeight();}
-$(function(){changeFrameHeight();});
+
+window.onresize=function(){
+    changeFrameWidth();
+}
+$(function(){
+    changeFrameWidth();
+});
+
+
+
+
+
+
 
